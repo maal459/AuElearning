916aba473f95558c8134e8c8185c8e01befbb179
maal459 <abdi.wahab.waberi@gmail.com>
2023-10-25 20:30:38 +0300

    first commit

---
+++ App/Pages/Login.js
@@ -0,0 +1,14 @@
+import { View, Image, Text } from 'react-native'
+import React from 'react'
+
+
+export default function Login() {
+  return (
+    <View>
+      <Image source={require('../../Assests/Images/camuud.jpg')}/>
+      
+      <Text> Amoud University E-learning App</Text>
+    </View>
+  )
+}
+
